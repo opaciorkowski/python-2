da4df606c236132c70b9453bd2660c9f3862669e
opaciorkowski <s25488@pjwstk.edu.pl>
2023-03-13 20:36:19 +0100

    edited readme

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,16 +1,16 @@
 a = "Python 2023"
 b = "Python 2023"
 c = "Python 2023"
-print(a==b)
-print(b==c)
+print("czy a==b? ",a==b)
+print("czy b==c? ",b==c)
 print("a "+hex(id(a)), type(a))
 print("b "+hex(id(b)), type(b))
 print("c "+hex(id(c)), type(c))
 
 print("punkt 3")
 c = "Java 11"
-print(a==b)
-print(b==c)
+print("czy a==b? ",a==b)
+print("czy b==c? ",b==c)
 print("a "+hex(id(a)), type(a))
 print("b "+hex(id(b)), type(b))
 print("c "+hex(id(c)), type(c))
